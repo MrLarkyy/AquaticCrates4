a70d854046e96d49fdf3641db1ff801d605a51e6
MrLarkyy_ <studiotadikl@gmail.com>
2025-12-31 20:10:33 +0100

    Set up project structure, add API module, and configure dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,26 @@
 plugins {
     kotlin("jvm") version "2.2.21"
+    id("io.github.revxrsal.bukkitkobjects") version "0.0.5"
+}
+
+bukkitKObjects {
+    classes.add("gg.aquatic.crates.CratesPlugin")
 }
 
 group = "gg.aquatic.klocale"
-version = "1.0-SNAPSHOT"
+version = "4.0.0"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    implementation(project(":api"))
     testImplementation(kotlin("test"))
 }
 
