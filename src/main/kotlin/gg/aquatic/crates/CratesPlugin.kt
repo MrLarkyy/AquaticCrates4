091ac9229c7aa5fabbb81dc91b621559e306ea13
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-08 18:00:49 +0100

    Add `/test` command for testing command execution functionality

--- src/main/kotlin/gg/aquatic/crates/CratesPlugin.kt
+++ src/main/kotlin/gg/aquatic/crates/CratesPlugin.kt
@@ -1,6 +1,8 @@
 package gg.aquatic.crates
 
 import gg.aquatic.crates.input.InputHandler
+import gg.aquatic.kommand.command
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 object CratesPlugin : JavaPlugin() {
@@ -10,6 +12,15 @@ object CratesPlugin : JavaPlugin() {
 
     override fun onEnable() {
         InputHandler.initialize()
+
+        command("test") {
+            "test" {
+                execute<Player> {
+                    sender.sendMessage("Test command executed!")
+                    true
+                }
+            }
+        }
     }
 
     override fun onDisable() {
